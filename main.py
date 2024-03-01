6ea9e41d15849e5b242b0a698bf6ed369281a05b
deka4core <borodaenglish@gmail.com>
2024-03-01 12:59:31 +0300

    added UI Button and Frame

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import sys
 
+from PyQt6.QtGui import QFontDatabase
 from PyQt6.QtWidgets import QApplication
 from widgets import MainWindow
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+    QFontDatabase.addApplicationFont('resources/lcd.14.otf')  # Добавление шрифта в БД QT
     executable = MainWindow()
     executable.show()
 
--- widgets.py
+++ widgets.py
@@ -1,6 +1,8 @@
-from PyQt6.QtWidgets import QMainWindow, QPushButton, QGridLayout, QWidget, QLabel
+from PyQt6.QtWidgets import QMainWindow, QPushButton, QWidget, QLabel
 from summator import summator, number_to_decimal
-from constants import BUTTON_HSIZE, BUTTON_WSIZE, WINDOW_HSIZE, WINDOW_WSIZE, BITNESS
+from constants import BUTTON_HSIZE, BUTTON_WSIZE, WINDOW_HSIZE, WINDOW_WSIZE, BITNESS, BGROUP_LEFT_MARGIN, \
+    BGROUP_TOP_MARGIN, BUTTON_LEFT_MARGIN, BUTTON_TOP_MARGIN, LGROUP_LEFT_MARGIN, LGROUP_TOP_MARGIN, \
+    LABEL_LEFT_MARGIN, LABEL_HSIZE, LABEL_WSIZE
 
 import database
 
@@ -9,15 +11,12 @@ class MainWindow(QMainWindow):
     def __init__(self):
         """ Инициализация класса окна приложения"""
         super().__init__()
-        self.resize(WINDOW_WSIZE, WINDOW_HSIZE)
-
-        """ Настройка Layout """
-        self.gridlayout = QGridLayout(self)
-        self.gridlayout.setHorizontalSpacing(20)
+        self.setFixedSize(WINDOW_WSIZE, WINDOW_HSIZE)
+        self.setStyleSheet(open("resources/summator_ui.css").read())
 
         """ Настройка основного виджета"""
         widget = QWidget()
-        widget.setLayout(self.gridlayout)
+        self.setObjectName("CentralWidget")
         self.setCentralWidget(widget)
 
         """ Инициализация эл-тов интерфейса(кнопок и текста) """
@@ -25,18 +24,19 @@ class MainWindow(QMainWindow):
         for i in range(3):
             self.buttons.append(list())
             self.labels.append(QLabel("0", self))
-            self.gridlayout.addWidget(self.labels[i], i, BITNESS)
+            self.labels[i].setGeometry(LGROUP_LEFT_MARGIN + (i * LABEL_LEFT_MARGIN), LGROUP_TOP_MARGIN, LABEL_WSIZE,
+                                       LABEL_HSIZE)
             for j in range(BITNESS):
-                self.buttons[i].append(QPushButton("0", self))
-                self.buttons[i][j].setMaximumSize(BUTTON_WSIZE, BUTTON_HSIZE)
+                print(f"Button{i}{j} POS: {BGROUP_LEFT_MARGIN + (j * BUTTON_LEFT_MARGIN + BUTTON_WSIZE)}, "
+                      f"{BGROUP_TOP_MARGIN + (i * BUTTON_TOP_MARGIN)} SIZE: {BUTTON_WSIZE, BUTTON_HSIZE}")
+                self.buttons[i].append(QPushButton(self))
+                self.buttons[i][j].setGeometry(BGROUP_LEFT_MARGIN + (j * BUTTON_LEFT_MARGIN + BUTTON_WSIZE),
+                                               BGROUP_TOP_MARGIN + (i * BUTTON_TOP_MARGIN), BUTTON_WSIZE, BUTTON_HSIZE)
+                self.buttons[i][j].setCheckable(True)
                 if i <= 1:
-                    self.buttons[i][j].setCheckable(True)
-                    self.buttons[i][j].clicked.connect(self.change_button_text)
-                else:
-                    self.buttons[i][j].setFlat(True)
-                self.gridlayout.addWidget(self.buttons[i][j], i, j)
+                    self.buttons[i][j].clicked.connect(self.change_button_image)
 
-    def change_button_text(self):
+    def change_button_image(self):
         """ QEvent на нажатие по первым двум рядам кнопок
             Изменяет значения на кнопке, а так же обновляет результаты вычислений
         """
@@ -44,18 +44,17 @@ class MainWindow(QMainWindow):
         sender: QPushButton = self.sender()
 
         """ Обработка QEvent на QPushButton """
-        sender.setText("1") if sender.text() == "0" else sender.setText("0")
         if sender in self.buttons[0]:
             index = self.buttons[0].index(sender)
-            database.first_numbers[index] = int(sender.text())
+            database.first_numbers[index] = sender.isChecked()
         elif sender in self.buttons[1]:
             index = self.buttons[1].index(sender)
-            database.second_numbers[index] = int(sender.text())
+            database.second_numbers[index] = sender.isChecked()
 
         """ Вычисление результата и отображение на экране """
         database.third_numbers = summator(database.first_numbers, database.second_numbers)
         for i in range(BITNESS):
-            self.buttons[2][i].setText(str(database.third_numbers[i]))
+            self.buttons[2][i].setChecked(database.third_numbers[i])
         output_numbers = list(map(lambda x: number_to_decimal(x), [database.first_numbers,
                                                                    database.second_numbers,
                                                                    database.third_numbers]))
